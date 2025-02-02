c11dc8414d6b238cb46b7463ae473bf5e62f9c2c
nishanshaju22 <nishan.shaju@icloud.com>
2025-02-02 13:15:23 +0800

    base url updated

--- src/Services/notes_services.js
+++ src/Services/notes_services.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://127.0.0.1:5000/"
+const BASE_URL = "https://notesapp-backend-pwlu.onrender.com/"
 
 export const getNotes = async() => {
     try {
